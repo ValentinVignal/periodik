19c5951159817809c583ebbfa6ebdab62ac2becd
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-04-11 14:34:30 +0800

    :sparkles: Use path url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:periodik/firebase_options.dart';
 import 'package:periodik/router/router.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
