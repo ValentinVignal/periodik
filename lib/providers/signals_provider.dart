80d5bf2daa8b80533a815a1fe2ea9ed5d557ae1b
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-09-16 23:26:37 +0800

    :goal_net: Catch firebase exception in signals provider

--- lib/providers/signals_provider.dart
+++ lib/providers/signals_provider.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:periodik/models/signal.dart';
 import 'package:periodik/providers/settings_provider.dart';
@@ -7,22 +8,30 @@ final signalsProvider = StreamProvider.autoDispose<List<Signal>>(
   (ref) async* {
     final displayHiddenSignals =
         ref.watch(settingsProvider).valueOrNull?.displayHiddenSignals ?? false;
-    await for (final signals
-        in Collections.signals.orderBy('name').snapshots()) {
-      yield signals.docs.map((document) {
-        return Signal.fromJsonDocument(
-          document.id,
-          document.data(),
-        );
-      }).where(
-        (signal) {
-          if (displayHiddenSignals) {
-            return true;
-          } else {
-            return !signal.hidden;
-          }
-        },
-      ).toList();
+    try {
+      await for (final signals
+          in Collections.signals.orderBy('name').snapshots()) {
+        yield signals.docs.map((document) {
+          return Signal.fromJsonDocument(
+            document.id,
+            document.data(),
+          );
+        }).where(
+          (signal) {
+            if (displayHiddenSignals) {
+              return true;
+            } else {
+              return !signal.hidden;
+            }
+          },
+        ).toList();
+      }
+    } on FirebaseAuthException catch (error) {
+      if (error.code == 'permission-denied') {
+        // This can happen when the user logs out.
+      } else {
+        rethrow;
+      }
     }
   },
   dependencies: [
