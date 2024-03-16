497a70964b81614d57cc1f0bbb6291bfa8f28eb9
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-03-16 19:05:11 +0800

    :package: Build runner

--- lib/models/signal_with_points.freezed.dart
+++ lib/models/signal_with_points.freezed.dart
@@ -12,7 +12,7 @@ part of 'signal_with_points.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 SignalWithPoints _$SignalWithPointsFromJson(Map<String, dynamic> json) {
   return _SignalWithPoints.fromJson(json);
@@ -145,7 +145,7 @@ class _$SignalWithPointsImpl extends _SignalWithPoints {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$SignalWithPointsImpl &&
